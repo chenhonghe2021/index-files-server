f0783b17c07d298542ba8d4aef99d8e956b8db36
chh <92646119+chenhonghe2021@users.noreply.github.com>
2025-07-03 01:06:44 +0800

    Update start.sh

--- nginx/start.sh
+++ nginx/start.sh
@@ -2,6 +2,7 @@ docker compose down
 docker compose build
 docker compose up -d
 
-echo "✅ Nginx文件服务器: http://localhost:18088"
+echo "✅ Nginx文件服务器: http://localhost:18180"
 echo "✅ Alist文件管理器: http://localhost:5744"
-echo "🔑 获取Alist管理员密码: docker exec -it openlist-file-server ./openlist admin"
+echo "🔑 随机生成openlist管理员密码: docker exec -it openlist-file-server ./openlist admin random" 
+echo "🔑 设置openlist管理员新密码: docker exec -it openlist-file-server ./openlist admin set 123admin" # 手动设置一个密码,`123admin`是指你需要设置的密码
